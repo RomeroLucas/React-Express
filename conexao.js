d3602ee6d059619aa3befd40665c1e014964d0df
RomeroLucas <40761894+RomeroLucas@users.noreply.github.com>
2021-01-16 20:10:43 -0300

    hospedar no cleaver

--- conexao.js
+++ conexao.js
@@ -1,10 +1,10 @@
+  
 const mysql = require('mysql')
-
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'lojarecode'
+    host : 'bcb4zitmaou2stkgdpv6-mysql.services.clever-cloud.com',
+    user : 'utqonpx5q16xfxeg',
+    password : 'Pf2cvnak5D4W8rQj7aR0',
+    database : 'bcb4zitmaou2stkgdpv6'
 })
 
 module.exports = conn
